63311381bda178d59bf02f9e102ee400fad8e9ab
Csillag61 <csillagok@gmail.com>
2025-05-11 11:09:23 -0400

    fixed column number

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,23 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   const table = document.querySelector('table');
-  
+
   if (!table) {
+    // eslint-disable-next-line no-console
     console.error('Table not found');
+
     return;
   }
 
   const rows = table.querySelectorAll('tr');
-  
-  rows.forEach(row => {
+
+  rows.forEach((row) => {
     const cells = row.querySelectorAll('th, td');
 
     if (cells.length > 1) {
       const clonedCell = cells[1].cloneNode(true);
 
-      row.insertBefore(clonedCell, cells[cells.length - 1]);
+      row.insertBefore(clonedCell, cells[2]);
     }
   });
 });
